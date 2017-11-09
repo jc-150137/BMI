a41c95dc760ab814c96807a3b2cde7fa588802c5
駿介 髙嶋 <150137@jc-21.jp>
2017-11-09 16:07:39 +0900

    初回

--- BMI_PCL/BMI_PCL/MainPage.xaml.cs
+++ BMI_PCL/BMI_PCL/MainPage.xaml.cs
@@ -5,13 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace BMI_PCL
+namespace BMI
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double hcm = double.Parse(this.height.Text);    //身長(cm or m)
+            double w = double.Parse(this.weight.Text);      //体重(kg)
+            double hm = hcm;
+            if (hcm > 5) //cm単位で入力された場合
+            {
+                hm = hcm / 100;
+            }
+            else        //m単位で入力された場合
+            {
+                hcm = hm * 100;
+            }
+
+            double bmi = w / (hm * hm);
+
+            string ans = "";
+            if (bmi < 16)
+            {
+                ans = "瘦せすぎ";
+            }
+            else if (bmi < 17)
+            {
+                ans = "瘦せ";
+            }
+            else if (bmi < 18.5)
+            {
+                ans = "瘦せぎみ";
+            }
+            else if (bmi < 25)
+            {
+                ans = "普通";
+            }
+            else if (bmi < 30)
+            {
+                ans = "肥満ぎみ";
+            }
+            else if (bmi < 35)
+            {
+                ans = "肥満";
+            }
+            else if (bmi >= 35)
+            {
+                ans = "過剰肥満";
+            }
+            else
+            {
+                ans = "異常";
+            }
+
+            DisplayAlert("BMI計算", "高さ=" + hcm.ToString() + "cm 重さ=" + w.ToString() + "kg 結果:" + ans, "OK");
+
         }
     }
 }
